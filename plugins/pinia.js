400dcb09000ae6fe9e892127535d843dab692682
Caleb Ebers <calebebers@gmail.com>
2025-08-07 14:05:03 -0500

    removed consoleStore and eventBusStore

--- plugins/pinia.js
+++ plugins/pinia.js
@@ -1,11 +1,9 @@
-import { useConsoleStore } from "~/store/console-store";
 import { useSvgStore } from "~/store/svg-store";
 
 export default defineNuxtPlugin(({ $pinia }) => {
   return {
     provide: {
       svgStore: useSvgStore($pinia),
-      consoleStore: useConsoleStore($pinia),
     },
   };
 });
