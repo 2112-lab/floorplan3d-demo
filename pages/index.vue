ee19302f92ba029d19119ef78ad2966bb8648097
Caleb Ebers <calebebers@gmail.com>
2025-08-07 12:56:35 -0500

    moved FP3D-00-07.svg and other renewed svg samples to inkscape-samples

--- pages/index.vue
+++ pages/index.vue
@@ -336,7 +336,7 @@ export default {
         }
         
         // Fetch the SVG file from the public directory
-        const response = await fetch('/floorplan-import-samples/FP3D-00-07.svg');
+        const response = await fetch('/inkscape-samples/FP3D-00-07.svg');
         if (!response.ok) {
           throw new Error(`Failed to fetch SVG file: ${response.status}`);
         }
