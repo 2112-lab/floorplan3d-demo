ca7b9cf700c4732f9a9a681295fa8ded0611f3bf
Caleb Ebers <calebebers@gmail.com>
2025-08-18 18:35:22 -0500

    fix: update title in nuxt.config.ts and remove border from ThreejsRenderer style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: 'Floorplan3D',
+      title: 'Floorplan3D Demo',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
